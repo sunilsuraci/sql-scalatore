b12c0191f1c8df83c91e00e92d89e5f88150a128
sunilsuraci <suracisunil@gmail.com>
2022-11-16 12:03:23 +0100

    es. n 2 completo

--- esercizio.sql
+++ esercizio.sql
@@ -5,6 +5,9 @@ WHERE scalatore.cf = scalata.scalatore
 
 --N2 Per	ogni	scalatore	nato	prima	del	1980,	calcolare	tutti	i	continenti	in	cui	ha	effettuato	una	scalata,	ordinando	il	risultato	per	codice	fiscale	e,	a	parità	di	codice	fiscale,	per	il	nome	del	continente
 
-SELECT Distinct scalatore.annonascita, scalatore.cf, scalata.scalatore, nazione.nome
+SELECT Distinct scalatore.annonascita, scalatore.cf, scalata.scalatore, nazione.nome, scalata.nazione
 FROM scalatore, scalata, nazione
 WHERE scalatore.annonascita < 1980 and scalatore.cf = scalata.scalatore and nazione.nome = scalata.nazione
+order by scalatore.cf, scalata.nazione
+
+--N3 Calcolare	le	nazioni	(mostrando,	per	ciascuna,	anche	il	continente)	nelle	quali	è	stata	effettuata	almeno	una	scalata	da	uno	scalatore	minorenne.
