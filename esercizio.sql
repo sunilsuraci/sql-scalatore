e4bd0e01a4ce387f38f7d8777f638549d3847c30
sunilsuraci <suracisunil@gmail.com>
2022-11-16 11:21:58 +0100

    inizio es. n1

--- esercizio.sql
+++ esercizio.sql
@@ -1,1 +1,4 @@
 --N1 Calcolare	l'insieme	(non	il	multi-insieme)	delle	coppie	(A,B)	tali	che	A	è	uno	scalatore	e	B	è	un	continente	in	cui	A	ha	effettuato	una	scalata.S
+select count(*)
+from  scalatore, scalata
+WHERE scalatore.cf = scalata.scalatore
