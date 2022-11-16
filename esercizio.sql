61e7aa6ed128d0ee98de5a9011e5f6fa04976bb0
sunilsuraci <suracisunil@gmail.com>
2022-11-16 11:30:49 +0100

    correzione es. n1

--- esercizio.sql
+++ esercizio.sql
@@ -1,4 +1,6 @@
 --N1 Calcolare	l'insieme	(non	il	multi-insieme)	delle	coppie	(A,B)	tali	che	A	è	uno	scalatore	e	B	è	un	continente	in	cui	A	ha	effettuato	una	scalata.S
-select count(*)
+select Distinct scalatore.cf, scalata.scalatore
 from  scalatore, scalata
 WHERE scalatore.cf = scalata.scalatore
+
+--N2 Per	ogni	scalatore	nato	prima	del	1980,	calcolare	tutti	i	continenti	in	cui	ha	effettuato	una	scalata,	ordinando	il	risultato	per	codice	fiscale	e,	a	parità	di	codice	fiscale,	per	il	nome	del	continente
